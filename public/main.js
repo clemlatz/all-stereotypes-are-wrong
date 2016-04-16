31e0b17d820fc4a22755cbe9654557c40c572c6d
Clement Bourgoin <cb@nokto.net>
2016-04-16 11:56:34 +0200

    Add click zones and animations

--- public/main.js
+++ public/main.js
@@ -6,16 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
       return response.json();
     }).then(function(json) {
 
+      const termsElement = document.querySelector('#terms');
       const termAElement = document.querySelector('#termA');
       const termBElement = document.querySelector('#termB');
-      const termBResult = document.querySelector('#termB');
+      const termBResult  = document.querySelector('#termB');
       const term1Element = document.querySelector('#term1');
       const term2Element = document.querySelector('#term2');
+      const leftZone     = document.querySelector('#leftZone');
+      const rightZone    = document.querySelector('#rightZone');
 
       termAElement.innerHTML = json[0][0];
       termBElement.innerHTML = json[0][1];
       term1Element.innerHTML = json[1][0];
       term2Element.innerHTML = json[1][1];
+
+      leftZone.addEventListener('mouseenter', function() {
+        terms.classList.add('leftChoice');
+      });
+      leftZone.addEventListener('mouseleave', function() {
+        terms.classList.remove('leftChoice');
+      });
+      rightZone.addEventListener('mouseenter', function() {
+        terms.classList.add('rightChoice');
+      });
+      rightZone.addEventListener('mouseleave', function() {
+        terms.classList.remove('rightChoice');
+      });
     })
 
 })
