fa742de60de53cc994ccb799e96bafb77c0d69fa
JohnNorad <alexis.moroz@gmail.com>
2016-04-16 15:31:18 +0200

    Update couples.js

--- data/couples.js
+++ data/couples.js
@@ -71,14 +71,74 @@ secondTerm = new Term('white', 'blanc');
 couple = new Couple('black-white', firstTerm, secondTerm, false, false);
 couples.addCouple(couple);
 
+firstTerm = new Term('smart', 'intelligent');
+secondTerm = new Term('dumb', 'bête');
+couple = new Couple('smart-dumb', firstTerm, secondTerm, false, true);
+couples.addCouple(couple);
+
+firstTerm = new Term('kind', 'gentil');
+secondTerm = new Term('mean', 'méchant');
+couple = new Couple('kind-mean', firstTerm, secondTerm, false, true);
+couples.addCouple(couple);
+
 firstTerm = new Term('leftist', 'de gauche');
 secondTerm = new Term('rightist', 'de droite');
 couple = new Couple('leftist-rightist', firstTerm, secondTerm, false, false);
 couples.addCouple(couple);
 
+firstTerm = new Term('vulgar', 'vulgaire');
+secondTerm = new Term('refined', 'raffiné');
+couple = new Couple('vulgar-refined', firstTerm, secondTerm, false, true);
+couples.addCouple(couple);
+
 firstTerm = new Term('vegeterian', 'végétarien');
 secondTerm = new Term('meat-eater', 'omnivore');
 couple = new Couple('vegetarian-meat-eater', firstTerm, secondTerm, false, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('smoker', 'fumeur');
+secondTerm = new Term('non-smoker', 'non-fumeur');
+couple = new Couple('smoker-non-smoker', firstTerm, secondTerm, true, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('young', 'jeune');
+secondTerm = new Term('old', 'vieux');
+couple = new Couple('young-old', firstTerm, secondTerm, false, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('manual', 'manuel');
+secondTerm = new Term('intellectual', 'intellectuel');
+couple = new Couple('manual-intellectual', firstTerm, secondTerm, false, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('shaved', 'rasé');
+secondTerm = new Term('hairy', 'poilu');
+couple = new Couple('shaved-hairy', firstTerm, secondTerm, true, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('happy', 'heureux');
+secondTerm = new Term('sad', 'malheureux');
+couple = new Couple('happy-sad', firstTerm, secondTerm, false, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('tall', 'grand');
+secondTerm = new Term('small', 'petit');
+couple = new Couple('tall-small', firstTerm, secondTerm, false, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('fat', 'gros');
+secondTerm = new Term('skinny', 'maigre');
+couple = new Couple('fat-skinny', firstTerm, secondTerm, false, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('original', 'original');
+secondTerm = new Term('conformist', 'conformiste');
+couple = new Couple('original-conformist', firstTerm, secondTerm, true, false);
+couples.addCouple(couple);
+
+firstTerm = new Term('believer', 'croyant');
+secondTerm = new Term('atheist', 'athée');
+couple = new Couple('believer-atheist', firstTerm, secondTerm, false, false);
 couples.addCouple(couple);
 
 
