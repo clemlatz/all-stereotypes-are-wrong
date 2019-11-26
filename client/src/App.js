df9d1eea03cc3fcb8d8d487eea8096c1d368a6e2
Clément Bourgoin <c@iwzr.fr>
2019-11-26 14:11:32 +0100

    Import old client structure into React App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="main">
+        <div id="header">
+          <h1>All Stereotypes Are Wrong</h1>
+          <p id="tagline">Choose the most common stereotype</p>
+        </div>
+
+        <div id="social">
+          <a
+            href="https://twitter.com/search?q=%23AllStereotypesAreWrong"
+            target="_blank"
+          >
+            <span class="fa fa-twitter-square"></span>
+          </a>{' '}
+          <a
+            href="https://github.com/iwazaru/all-stereotypes-are-wrong"
+            target="_blank"
+          >
+            <span class="fa fa-github-square"></span>
+          </a>
+        </div>
+
+        <div id="stats">
+          <span class="count"></span> stereotypes so far
+        </div>
+
+        <div class="current line">
+          <div class="left">
+            <span class="round"></span>
+          </div>
+          <div id="terms" class="terms"></div>
+          <div class="right"></div>
+        </div>
+
+        <div id="answers"></div>
+      </div>
     </div>
   );
 }
