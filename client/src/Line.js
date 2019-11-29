df2a948685c4ec7206c00cbc78e103dd3b72ffe7
Clement Bourgoin <c@iwzr.fr>
2019-11-29 14:18:24 +0100

    Send answer to server

--- client/src/Line.js
+++ client/src/Line.js
@@ -2,34 +2,76 @@ import React, { useState, useEffect } from 'react';
 
 import './Line.css';
 
+async function sendAnswer(
+  couple1,
+  couple2,
+  association1,
+  association2,
+  token,
+  setIsLoading
+) {
+  try {
+    setIsLoading(true);
+    const response = await fetch('/answer', {
+      method: 'POST',
+      body: JSON.stringify({
+        association1,
+        association2,
+        couple1,
+        couple2,
+        token,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setIsLoading(false);
+    const json = await response.json();
+    if (json.error) {
+      alert(json.error);
+    } else {
+      // const results = JSON.parse(req.response);
+      // addAnswer(terms, results, side);
+    }
+  } catch (error) {
+    alert(`Error: ${error}`);
+  }
+}
+
 function Line({ round }) {
-  const [currentCouples, setCurrentCouples] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
   const [currentChoice, setCurrentChoice] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Get question couples
   useEffect(() => {
     async function fetchCouples() {
       const response = await fetch('/couples');
-      const result = await response.json();
-      setCurrentCouples(result.couples);
+      const { couples, token } = await response.json();
+      setCurrentQuestion({ couples, token });
     }
     fetchCouples();
   }, []);
 
-  if (currentCouples === null) {
+  if (currentQuestion === null) {
     return null;
   }
 
-  const termA = currentCouples[0].firstTerm.en;
-  const termB = currentCouples[0].secondTerm.en;
-  const term1 = currentCouples[1].firstTerm.en;
-  const term2 = currentCouples[1].secondTerm.en;
+  const { couples, token } = currentQuestion;
+  const [couple1, couple2] = couples;
+  const termA = couple1.firstTerm.en;
+  const termB = couple1.secondTerm.en;
+  const term1 = couple2.firstTerm.en;
+  const term2 = couple2.secondTerm.en;
+
+  let lineClass = ['line'];
+  isLoading && lineClass.push('loading');
 
   let termsClass = ['terms'];
   termsClass.push(`${currentChoice}-choice`);
 
   return (
-    <div className="line">
+    <div className={lineClass.join(' ')}>
       <div className="left">
         <span className="round">{round}/10</span>
       </div>
@@ -44,11 +86,31 @@ function Line({ round }) {
           className="zone left-zone"
           onMouseEnter={() => setCurrentChoice('left')}
           onMouseLeave={() => setCurrentChoice(null)}
+          onClick={() =>
+            sendAnswer(
+              couple1.id,
+              couple2.id,
+              [termA, term1].join(),
+              [termB, term2].join(),
+              token,
+              setIsLoading
+            )
+          }
         ></div>
         <div
           className="zone right-zone"
           onMouseEnter={() => setCurrentChoice('right')}
           onMouseLeave={() => setCurrentChoice(null)}
+          onClick={() =>
+            sendAnswer(
+              couple1.id,
+              couple2.id,
+              [termA, term2].join(),
+              [termB, term1].join(),
+              token,
+              setIsLoading
+            )
+          }
         ></div>
       </div>
       <div className="right"></div>
