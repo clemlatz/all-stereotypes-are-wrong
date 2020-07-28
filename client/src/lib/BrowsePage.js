7c1844f0a5c4a29d9655eae318873c975a8853d8
Clément Bourgoin <c@iwzr.fr>
2020-07-28 14:11:25 +0200

    Display all couples on browse page

--- client/src/lib/BrowsePage.js
+++ client/src/lib/BrowsePage.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function BrowsePage({ browsePageClasses }) {
-  return <div className={browsePageClasses.join(' ')}>Browse stereotypes</div>;
+  const [couples, setCouples] = useState([]);
+
+  // Get couples
+  useEffect(() => {
+    async function fetchStats() {
+      const response = await fetch('/browse/couples');
+      const { couples } = await response.json();
+      setCouples(couples);
+    }
+    fetchStats();
+  }, []);
+
+  return (
+    <div className={browsePageClasses.join(' ')}>
+      {couples.map(couple => <p>{couple.id}</p>)}
+    </div>
+  );
 }
