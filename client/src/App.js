2a61cc36cf7bd75bfdf3b4c647423cd159c179e9
Clément Bourgoin <c@iwzr.fr>
2020-08-17 12:42:40 +0200

    Move ChooseApp page into a stand-alone component

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from 'react';
-
-import Line from './Line';
-import Question from './Question';
+import React, { useState } from 'react';
 import NavMenu from './NavMenu';
 
 import './App.css';
 import BrowsePage from './BrowsePage';
+import ChoosePage from './ChoosePage';
 
 function App() {
-  const [round, setRound] = useState(1);
-  const [lineNum, setLineNum] = useState(1);
   const [stereotypesCount, setStereotypesCount] = useState('...');
-  const [lines, setLines] = useState([], 'lines');
   const [currentPage, setCurrentPage] = useState('choose');
-
-  const [score, setScore] = useState(0);
-
-  // Get stereotypescount count
-  useEffect(() => {
-    async function fetchStats() {
-      const response = await fetch('/stats');
-      const stats = await response.json();
-      setStereotypesCount(stats.stereotypes);
-    }
-    fetchStats();
-  }, []);
 
   const choosePageClasses = ['page', 'choose-page'];
   const browsePageClasses = ['page', 'browse-page'];
@@ -66,47 +49,7 @@ function App() {
 
         <div id="stats">{stereotypesCount} stereotypes so far</div>
 
-        {/* Choose page */}
-        <div className={choosePageClasses.join(' ')}>
-          {/* Current line */}
-          <Question
-            round={round}
-            setStereotypesCount={setStereotypesCount}
-            setRound={setRound}
-            setLineNum={setLineNum}
-            lineNum={lineNum}
-            setLines={setLines}
-            setScore={setScore}
-            score={score}
-          ></Question>
-
-          {/* Answers list */}
-          <div className="answers">
-            {lines.map(
-              ({ lineNum, round, type, couples, choice, results, score }) => {
-                if (type === 'score') {
-                  return <Line key={lineNum} type="score" score={score} />;
-                }
-
-                const [couple1, couple2] = couples;
-                return (
-                  <Line
-                    key={lineNum}
-                    type="answer"
-                    round={round}
-                    couple1={couple1}
-                    couple2={couple2}
-                    currentChoice={choice}
-                    results={results}
-                    score={score}
-                  />
-                );
-              }
-            )}
-          </div>
-        </div>
-
-        {/* Browse page */}
+        <ChoosePage choosePageClasses={choosePageClasses} setStereotypesCount={setStereotypesCount} />
         <BrowsePage browsePageClasses={browsePageClasses} />
       </div>
     </div>
