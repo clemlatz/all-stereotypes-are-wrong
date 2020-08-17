18a98cb1bef86de7020bb6d09cec9f44ba9f5821
Clément Bourgoin <c@iwzr.fr>
2020-08-17 13:50:39 +0200

    Ask server for stats when both couples are selected

--- client/src/BrowsePage.js
+++ client/src/BrowsePage.js
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import CoupleSelector from './CoupleSelector';
 
+import './BrowsePage.css';
+
 export default function BrowsePage({ browsePageClasses }) {
   const [couples, setCouples] = useState([]);
+  const [selectedCouple1, setSelectedCouple1] = useState(null);
+  const [selectedCouple2, setSelectedCouple2] = useState(null);
+  const [, setStats] = useState(null);
 
   // Get couples
   useEffect(() => {
-    async function fetchStats() {
+    async function fetchCouple() {
       const response = await fetch('/browse/couples');
       const { couples } = await response.json();
       setCouples(couples);
     }
-    fetchStats();
+    fetchCouple();
   }, []);
+
+  useEffect(() => {
+    if (selectedCouple1 === null || selectedCouple2 === null) {
+      return;
+    }
+
+    async function fetchCouple() {
+      const response = await fetch(
+        `/browse/stats?couple1=${selectedCouple1}&couple2=${selectedCouple2}`
+      );
+      const { stats } = await response.json();
+      setStats(stats);
+    }
+    fetchCouple();
+  }, [selectedCouple1, selectedCouple2]);
+
+  function onCouple1Select({ value }) {
+    setSelectedCouple1(value);
+  }
+
+  function onCouple2Select({ value }) {
+    setSelectedCouple2(value);
+  }
 
   return (
     <div className={browsePageClasses.join(' ')}>
-      <CoupleSelector couples={couples} /> are{' '}
-      <CoupleSelector couples={couples} />
+      <CoupleSelector couples={couples} onSelect={onCouple1Select} /> are{' '}
+      <CoupleSelector couples={couples} onSelect={onCouple2Select} />
     </div>
   );
 }
