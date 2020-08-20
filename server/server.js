9764852a16022b98a77b720b1734a484f79c686f
Clément Bourgoin <c@iwzr.fr>
2020-08-20 14:21:08 +0200

    Add an API endpoint to return combination stats

--- server/server.js
+++ server/server.js
@@ -16,9 +16,28 @@ const app = express();
 app.use(express.json());
 app.use(express.static('../client/build'));
 
+async function getCombinationStats(combinationId) {
+  const combination = await Combination.findOne({
+    where: {
+      combination: combinationId,
+    },
+  });
+  const answers = await Answer.findAll({
+    where: {
+      combination: combinationId,
+    }
+  });
+
+  return {
+    combination: combination.combination,
+    total: combination.count,
+    answers: answers.map(({ answer, count }) => ({ answer, count })),
+  };
+}
+
 /** CHOOSE API */
 
-app.get('/couples', async function(request, response) {
+app.get('/couples', async function (request, response) {
   const couple1 = couples.getRandom();
   const couple2 = couples.getRandom(couple1);
 
@@ -27,20 +46,14 @@ app.get('/couples', async function(request, response) {
   response.json({ couples: [couple1, couple2], token: token.token });
 });
 
-app.get('/stats', async function(request, response) {
+app.get('/stats', async function (request, response) {
   const stats = await getStats();
   response.json(stats);
 });
 
-app.post('/answer', checkToken, async function(request, response) {
-  const association1 = request.body.association1
-    .split(',')
-    .sort()
-    .join(',');
-  const association2 = request.body.association2
-    .split(',')
-    .sort()
-    .join(',');
+app.post('/answer', checkToken, async function (request, response) {
+  const association1 = request.body.association1.split(',').sort().join(',');
+  const association2 = request.body.association2.split(',').sort().join(',');
   const answerContent = [association1, association2].sort().join(';');
 
   try {
@@ -76,17 +89,26 @@ app.post('/answer', checkToken, async function(request, response) {
       .status(200)
       .json({ count: answer.count, total: combination.count, stats });
   } catch (error) {
-    response.status(500).json({ error });
+    response.status(500).json({ error: error.message });
   }
 });
 
 /** BROWSE API */
 
-app.get('/browse/couples', async function(request, response) {
+app.get('/browse/couples', async function (request, response) {
   response.json({ couples: couples.getAll() });
 });
 
+app.get('/browse/stats', async function (request, response) {
+  try {
+    const combinationId = getCombinationId(request.query.couple1, request.query.couple2);
+    const stats = await getCombinationStats(combinationId);
+    response.json({ stats });
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
   process.stdout.write(`ASAW server is listening on port ${port}\n`);
 });
