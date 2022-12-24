7b94080b627c92c8226381bcc9a2c9b8126eed81
Clément Latzarus <c@iwzr.fr>
2022-12-24 14:59:44 +0100

    feat(client): add stats serverless functions

---
+++ client/netlify/functions/stats.js
@@ -0,0 +1,17 @@
+const statsController = require('../../../server/controllers/stats');
+
+exports.handler = async function () {
+  let responseContent;
+  const responseHandler = {
+    json: function(json) {
+      responseContent = JSON.stringify(json);
+    }
+  }
+
+  await statsController(undefined, responseHandler);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseContent),
+  };
+};
